79ce132128a5b86ee5f7e96fe71ea087b7749784
Gabriel Arias <gabrielpro2324g@gmail.com>
2026-02-21 17:41:21 -0500

    Información solicitada al cliente

---
+++ tarea1.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main () {
+  print ("Escriba su primer nombre");
+  String? nombre = stdin.readLineSync();
+
+  print ("Escriba su segundo nombre");
+  String? segundoNombre = stdin.readLineSync();
+
+ print ("Escriba su primer apellido");
+  String? apellido = stdin.readLineSync();
+
+  print ("Escriba su segundo apellido");
+  String? segundoApellido = stdin.readLineSync();
+
+  print ("Escriba su edad");
+  String? edad = stdin.readLineSync();
+
+  print ("Escriba el año en que nacio");
+  String? anonacimiento = stdin.readLineSync();
+
+}
